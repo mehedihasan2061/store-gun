4d8569b6126ead45ac36eeb762d373d31e70525d
mehedihasan2061 <mehedi.hasan206191@gmail.com>
2023-03-09 15:08:00 +0600

    style added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h2 className='text-3xl mt-4'>Hello everyone</h2>
             <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit distinctio consequatur tempora voluptate enim nulla perferendis itaque corrupti, cumque sunt laudantium iste magnam provident quo, quaerat magni sed voluptas dolorum?</p>
         </div>
